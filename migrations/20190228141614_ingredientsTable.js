0c2eb1767959921fc46435b17a18214465d3da4c
McNultyPT <mcnultypt@gmail.com>
2019-02-28 14:20:23 -0800

    creating ingredients table

---
+++ migrations/20190228141614_ingredientsTable.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('ingredients', function(tbl) {
+        tbl.increments();
+
+        tbl
+            .string('name', 128)
+            .notNullable()
+            .unique();
+
+        tbl
+            .integer('quantity')
+            .notNullable();
+
+        tbl.timestamps(true, true);
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('ingredients');
+};
