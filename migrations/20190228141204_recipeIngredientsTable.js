7f9e623f555fb882b7587c605e567c9e4198d7f1
McNultyPT <mcnultypt@gmail.com>
2019-02-28 14:15:43 -0800

    creating recipe_ingredients table to connect recipe and ingredients tables

---
+++ migrations/20190228141204_recipeIngredientsTable.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('recipe_ingredients', function(tbl) {
+        tbl
+            .integer('recipe_id')
+            .unsigned()
+            .references('id')
+            .inTable('recipes')
+            .onUpdate('CASCADE');
+
+        tbl
+            .integer('ingredients_id')
+            .unsigned()
+            .references('id')
+            .inTable('ingredients')
+            .onUpdate('CASCADE');
+
+        tbl.timestamps(true, true);
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('recipe_ingredients');
+};
