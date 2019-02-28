184f1fd8373090175ba5005d7f536bfa29ff51b1
McNultyPT <mcnultypt@gmail.com>
2019-02-28 15:59:13 -0800

    working on getDish by id method to return a dish and its recipes

--- recipe_book/recipe_book_access.js
+++ recipe_book/recipe_book_access.js
@@ -4,17 +4,22 @@ const db = knex(config.development);
 
 module.exports = {
     getDishes,
+    getDish,
 };
 
 function getDishes() {
     return db('dishes')
 }
 
-// function getDish(id) {
-//     return db('dishes')
-//         .where({ id })
-//         .first();
-// }
+function getDish(id) {
+    return db('dishes')
+        .where({ id })
+        .innerJoin('recipes', 'dishes.id', 'recipes.dish_id')
+}
+
+// select * dishes, recipes.name
+// from dishes
+// inner join recipes on dishes.id = recipes.dish_id
 
 // async function addDish(dish) {
 //     const [id] = await db('dishes').insert(dish);
--- recipe_book/recipe_book_router.js
+++ recipe_book/recipe_book_router.js
@@ -14,4 +14,20 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const dish = await getDish(id)
+
+    if (dish) {
+        res.status(200).json(dish);
+    } else {
+        res.status(404).json({ errorMessage: 'A dish with that ID does not exist.' });
+    }
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ error: 'Could not retrieve the dish.' });
+    }
+});
+
 module.exports = router;
